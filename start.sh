2ca3ee6d461f7c7609369f8a62376fca22ed05be
sagargit <ssagarbam@gmail.com>
2017-11-17 20:33:39 +0545

    Jenkinsfile modified

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ cd target/universal/
 echo '-- Unzipping distribution --'
 unzip car-advert-system-1.0.0 -d car-advert-system-1.0.0
 echo '-- Now staring application --'
-nohup car-advert-system-1.0.0/car-advert-system-1.0.0/bin/car-advert-system -Dreload=true -J-server -J-Xmx2048m -J-Xmx2048M -Dconfig.resource=application.conf -Dhttp.port=9005 > /dev/null 2>&1 & echo $! > RUNNING_PID
+nohup car-advert-system-1.0.0/car-advert-system-1.0.0/bin/car-advert-system -Dreload=true -J-server -J-Xmx2048m -J-Xmx2048M -Dhttp.port=9005 > /dev/null 2>&1 & echo $! > RUNNING_PID
 echo '-- Car advert system has been started successfully --'
