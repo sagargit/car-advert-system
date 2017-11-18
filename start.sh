fcc16773c277c41cace94e0dd2e6d7bd509dc17e
sagargit <ssagarbam@gmail.com>
2017-11-18 16:54:55 +0545

    Jenkinsfile modified

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ unzip car-advert-system-1.0.0 -d car-advert-system-1.0.0
 echo '-- Now staring application --'
 sudo chmod a+x car-advert-system-1.0.0/car-advert-system-1.0.0/bin/car-advert-system
 sudo nohup car-advert-system-1.0.0/car-advert-system-1.0.0/bin/car-advert-system -Dreload=true -J-server -J-Xmx2048m -J-Xmx2048M -Dhttp.port=9005 > $WORKSPACE/app.log 2>&1 & echo $! > save_pid.txt
-echo '-- Car advert system has been started successfully -----'
+echo '-- Car advert system has been started successfully--'
