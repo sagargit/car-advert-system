591e3b572278bab1500e89304eb63771c1c189b4
sagargit <ssagarbam@gmail.com>
2017-11-18 17:14:21 +0545

    Jenkinsfile modified

--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 echo '-- Going to kill the nohup bg process ---'
-sudo test -f target/universal/save_pid.txt && kill `cat target/universal/save_pid.txt` && sleep 5;
+sudo kill `cat target/universal/save_pid.txt` && sleep 5;
 sudo rm target/universal/save_pid.txt;
